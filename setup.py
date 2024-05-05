d7ebef6a742bf5f5417f9454f9c7eba87f216225
darreion <115027606+darreion@users.noreply.github.com>
2024-05-05 13:24:37 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='montecarlo',
+      version='1.0',
+      description='Montecarlo',
+      url='https://github.com/darreion/rzu5vw_ds5100_montecarlo',
+      author='Darreion Bailey',
+      author_email='rzu5vw@virginia.edu',
+      license='MIT',
+      packages=['montecarlo'])
